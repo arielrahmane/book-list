54d4c77ac4e01e39a4da36dbb246c81116d5e593
Ariel Rahmane <ariel@nomoss.co>
2021-09-19 19:18:56 +1000

    retrieved books data as json

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,40 @@
+require('dotenv').config()
 const fs = require('fs');
 const path = require( 'path' );
+interface Book {
+    title: string;
+    author: string;
+    files: [];
+}
 
-const AL = path.relative('' ,'/Users/arielrahmane/Documents/Ariel/Libros/Books/A-L');
-const MZ = path.relative('' ,'/Users/arielrahmane/Documents/Ariel/Libros/Books/m-z');
-let books = '';
-let booksObj = {};
+const AL = path.relative('' , process.env.AL_PATH);
+const MZ = path.relative('' ,process.env.MZ_PATH);
+let booksStr = '';
+let books = [] as Book[];
 
-const getBooks = (path: string, directory: string) => {
-    const source = `${path}/${directory}`;
-    return fs.readdirSync(source, { withFileTypes: true })
+function getData(source: string): void {
+    fs.readdirSync(source, { withFileTypes: true })
         .filter((dirent: any) => dirent.isDirectory())
-        .map((dirent: any) => dirent.name);
+        .map((dir: any) => {
+            const authorName = dir.name;
+            const authorDir = source + '/' + authorName;
+            fs.readdirSync(authorDir, { withFileTypes: true })
+                .filter((dirent: any) => dirent.isDirectory())
+                .map((book: any) => {
+                    const bookDir = authorDir + '/' + book.name;
+                    let data = {
+                        title: book.name,
+                        author: authorName,
+                        files: fs.readdirSync(bookDir, { withFileTypes: true }).map((file: any) => file.name)
+                    } as Book;
+                    books.push(data);
+                })
+        })
 }
 
-const getAuthors = (source: string) => {
-    return new Promise<void>((resolve, reject) => {
-        fs.readdirSync(source, { withFileTypes: true })
-            .filter((dirent: any) => dirent.isDirectory())
-            .map((dirent: any) => {
-                const b = getBooks(source, dirent.name);
-                b.map((i: string) => books += i + `\n`);
-            });
-        resolve();
-    })
-}
-
-getAuthors(AL);
-getAuthors(MZ);
-
-
-setTimeout(() => {
-    fs.writeFile('books.txt', books, function (err: any) {
-        if (err) return console.log(err);
-        console.log('succesfully written');
-    });
-}, 2000);
+getData(AL);
+getData(MZ);
+fs.writeFile('books.json', JSON.stringify(books), function (err: any) {
+    if (err) return console.log(err);
+    console.log('succesfully written');
+});
